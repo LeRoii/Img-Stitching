060a67ec3066ece0877efe7be282f8274e812bad
wzhpanging@163.com <wzhpanging@163.com>
2018-01-29 00:04:22 +0800

    .

--- src/fsmstate.cpp
+++ src/fsmstate.cpp
@@ -219,7 +219,8 @@ namespace panoAPP{
             pPanocam->detect(frame);
         if(pPanocam->sysStatus().crossTrigger)
             pPanocam->drawCross(frame);
-        if(pPanocam->sysStatus().saveTrigger)
+        // if(pPanocam->sysStatus().saveTrigger)
+        if(1)
         {
             // cv::resize(frame, frame, cv::Size(1280, 720));
             pPanocam->saveAndSend(frame);
